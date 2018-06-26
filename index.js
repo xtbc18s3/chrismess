20042f64d1e8bd59c367be928b1e260cf94e2fcc
Dave Strus <dave@getfretless.com>
2018-06-26 10:48:08 -0400

    Add flicks to a div as they're submitted

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const form = document.querySelector('form#flickForm')
 const changeHeading = function(ev) {
   ev.preventDefault()
   const f = ev.target
-  const heading = document.querySelector('h1')
-  heading.textContent = f.flickName.value
+  const flicksDiv = document.querySelector('#flicks')
+  flicksDiv.textContent += ' ' + f.flickName.value
 }
 
 form.addEventListener('submit', changeHeading)
