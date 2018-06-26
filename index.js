63877d40a2231002124dc42421a3223c85440a3e
Dave Strus <dave@getfretless.com>
2018-06-26 14:52:54 -0400

    Update the heading with text from the form

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const form = document.querySelector('form#flickForm')
 
 const updateHeading = function(ev) {
   ev.preventDefault()
+  const f = ev.target
+
+  const flickName = f.flickName.value
 
   const heading = document.querySelector('h1')
-  heading.textContent = 'Click!'
+  heading.textContent = flickName
 }
 
 form.addEventListener('submit', updateHeading)
