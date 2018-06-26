dbfae1c133868fbc1c6717e3b7d5cd0b85bcdf06
Dave Strus <dave@getfretless.com>
2018-06-26 16:22:04 -0400

    Add flicks using createElement and appendChild

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const updateHeading = function(ev) {
   const f = ev.target
 
   const flickName = f.flickName.value
+  const item = document.createElement('li')
+  item.textContent = flickName
 
   const list = document.querySelector('#flicks')
-  list.innerHTML += `<li>${flickName}</li>`
+  list.appendChild(item)
 
   f.reset()
 }
