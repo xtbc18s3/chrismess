86e7450c2b7303978ed1aa4367f04ed5fb9b07a5
Dave Strus <dave@getfretless.com>
2018-06-26 11:03:27 -0400

    Use a template literal

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const form = document.querySelector('form#flickForm')
 const changeHeading = function(ev) {
   ev.preventDefault()
   const f = ev.target
+
+  const flickName = f.flickName.value
+
   const flicksDiv = document.querySelector('#flicks')
-  flicksDiv.innerHTML += '<p>' + f.flickName.value + '</p>'
+  flicksDiv.innerHTML += `<p>${flickName}</p>`
 }
 
 form.addEventListener('submit', changeHeading)
