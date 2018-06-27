1e836ef021f81d81524188eebaa7453ae6dda246
Dave Strus <dave@getfretless.com>
2018-06-27 14:24:09 -0400

    Add class names to the elements we create

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const handleSubmit = function(ev) {
 
   const flickName = f.flickName.value
   const flickSpan = document.createElement('span')
+  flickSpan.classList.add('name')
   flickSpan.textContent = flickName
 
   const chrisName = f.chrisName.value
   const chrisSpan = document.createElement('span')
+  chrisSpan.classList.add('chris')
   chrisSpan.textContent = chrisName
 
   const item = document.createElement('li')
+  item.classList.add('flick')
   item.appendChild(flickSpan)
   item.appendChild(chrisSpan)
 
