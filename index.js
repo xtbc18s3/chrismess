5360707b518ffd51fb2bdc5b1cbe20bc2bfc0c8b
Dave Strus <dave@getfretless.com>
2018-06-27 11:52:26 -0400

    Use an ES6 class and arrow functions

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
-const form = document.querySelector('form#flickForm')
-
-const renderProperty = function(name, value) {
-  const span = document.createElement('span')
-  span.classList.add(name)
-  span.textContent = value
-  return span
-}
-
-const renderItem = function(flick) {
-  const item = document.createElement('li')
-  item.classList.add('flick')
-
-  // get the list of properties
-  const properties = Object.keys(flick)
-
-  // loop over the properties
-  properties.forEach(function(propertyName) {
-    // build a span, and append it to the list
-    const span = renderProperty(propertyName, flick[propertyName])
-    item.appendChild(span)
-  })
-
-  return item
-}
-
-const handleSubmit = function(ev) {
-  ev.preventDefault()
-  const f = ev.target
-
-  const flick = {
-    name: f.flickName.value,
-    chris: f.chrisName.value,
+class App {
+  constructor() {
+    const form = document.querySelector('form#flickForm')
+    form.addEventListener('submit', (ev) => {
+      ev.preventDefault()
+      this.handleSubmit(ev)
+    })
   }
 
-  const item = renderItem(flick)
+  renderProperty(name, value) {
+    const span = document.createElement('span')
+    span.classList.add(name)
+    span.textContent = value
+    return span
+  }
 
-  const list = document.querySelector('#flicks')
-  list.appendChild(item)
+  renderItem(flick) {
+    const item = document.createElement('li')
+    item.classList.add('flick')
 
-  f.reset()
+    // get the list of properties
+    const properties = Object.keys(flick)
+
+    // loop over the properties
+    properties.forEach((propertyName) => {
+      // build a span, and append it to the list
+      const span = this.renderProperty(propertyName, flick[propertyName])
+      item.appendChild(span)
+    })
+
+    return item
+  }
+
+  handleSubmit(ev) {
+    const f = ev.target
+
+    const flick = {
+      name: f.flickName.value,
+      chris: f.chrisName.value,
+    }
+
+    const item = this.renderItem(flick)
+
+    const list = document.querySelector('#flicks')
+    list.appendChild(item)
+
+    f.reset()
+  }
 }
 
-form.addEventListener('submit', handleSubmit)
+const app = new App()
